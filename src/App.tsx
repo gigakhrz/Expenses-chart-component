2cae4fac1cc6ce176be1d23dfcd651ec76091728
gigakhrz <kharazishvilig13@gmail.com>
2023-05-04 14:33:34 +0400

    create components and Globalstyle

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import GlobalStyles from "./GlobalStyles";
+
+function App() {
+  return (
+    <div>
+      <GlobalStyles />
+    </div>
+  );
+}
+
+export default App;
